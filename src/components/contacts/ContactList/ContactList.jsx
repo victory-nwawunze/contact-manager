4784880f01ed1fbbbe42d05b76f55aaef54b5722
victory-nwawunze <victorynwawunze@gmail.com>
2022-05-06 13:23:34 -0700

    creating cards

--- src/components/contacts/ContactList/ContactList.jsx
+++ src/components/contacts/ContactList/ContactList.jsx
@@ -11,7 +11,7 @@ let ContactList = () => {
           <div className="grid">
             <div className="row">
               <div className="col">
-                <p className="h3">
+                <p className="h3 fw-bold">
                   Contact Manager
                   <Link to={"/contacts/add"} className="btn btn-primary ms-3">
                     <i className="fa fa-plus-circle me-2" /> New
@@ -47,6 +47,59 @@ let ContactList = () => {
                     </div>
                   </div>
                 </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Cards */}
+      <section className="contact-list">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <div className="card">
+                <div className="card-body">
+                  <div className="row align-items-center d-flex justify-content-around">
+                    <div className="col-md-4">
+                      <img
+                        className="img-fluid contact-img"
+                        src="http://assets.stickpng.com/images/585e4bcdcb11b227491c3396.png"
+                      />
+                    </div>
+                    <div className="col-md-7">
+                      <ul className="list-group">
+                        <li className="list-group-item list-group-item-action">
+                          Name : <span className="fw-bold">Rajan</span>
+                        </li>
+                        <li className="list-group-item list-group-item-action">
+                          Mobile : <span className="fw-bold">0202888333</span>
+                        </li>
+                        <li className="list-group-item list-group-item-action">
+                          Email :{" "}
+                          <span className="fw-bold">Rajan@gmail.com</span>
+                        </li>
+                      </ul>
+                    </div>
+                    <div className="col-md-1 d-flex flex-column align-items-center">
+                      <Link
+                        to={`/contacts/view/:contactId`}
+                        className="btn btn-warning my-1"
+                      >
+                        <i className="fa fa-eye" />
+                      </Link>
+                      <Link
+                        to={`/contacts/edit/:contactId`}
+                        className="btn btn-primary  my-1"
+                      >
+                        <i className="fa fa-pen" />
+                      </Link>
+                      <button className="btn btn-danger  my-1">
+                        <i className="fa fa-trash" />
+                      </button>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
