61e44b2320b5d2849320f5877c8214fdedc03a18
victory-nwawunze <victorynwawunze@gmail.com>
2022-05-06 15:44:51 -0700

    created view  page

--- src/components/contacts/ContactList/ContactList.jsx
+++ src/components/contacts/ContactList/ContactList.jsx
@@ -102,6 +102,52 @@ let ContactList = () => {
                 </div>
               </div>
             </div>
+
+            <div className="col-md-6">
+              <div className="card">
+                <div className="card-body">
+                  <div className="row align-items-center d-flex justify-content-around">
+                    <div className="col-md-4">
+                      <img
+                        className="img-fluid contact-img"
+                        src="http://assets.stickpng.com/images/585e4bcdcb11b227491c3396.png"
+                      />
+                    </div>
+                    <div className="col-md-7">
+                      <ul className="list-group">
+                        <li className="list-group-item list-group-item-action">
+                          Name : <span className="fw-bold">Rajan</span>
+                        </li>
+                        <li className="list-group-item list-group-item-action">
+                          Mobile : <span className="fw-bold">0202888333</span>
+                        </li>
+                        <li className="list-group-item list-group-item-action">
+                          Email :{" "}
+                          <span className="fw-bold">Rajan@gmail.com</span>
+                        </li>
+                      </ul>
+                    </div>
+                    <div className="col-md-1 d-flex flex-column align-items-center">
+                      <Link
+                        to={`/contacts/view/:contactId`}
+                        className="btn btn-warning my-1"
+                      >
+                        <i className="fa fa-eye" />
+                      </Link>
+                      <Link
+                        to={`/contacts/edit/:contactId`}
+                        className="btn btn-primary  my-1"
+                      >
+                        <i className="fa fa-pen" />
+                      </Link>
+                      <button className="btn btn-danger  my-1">
+                        <i className="fa fa-trash" />
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </section>
